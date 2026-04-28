7fe917ed491e7e8be65e5405fd90eff321dddaf8
sheryn7 <sherynbatrisyia@gmail.com>
2026-04-28 11:09:43 +0800

    task 2 change position z to x

--- NewComponent.cs
+++ NewComponent.cs
@@ -3,23 +3,35 @@ using UnityEngine;
 public class NewComponent : MonoBehaviour
 {
 
-    Vector3 valueToMove = new Vector3(0, 0, 0.01f); // moving Z-axis
+    // Vector3 valueToMove = new Vector3(0, 0, 0.01f); // moving Z-axis
+    Vector3 valueToMove = new Vector3(0.01f, 0, 0); // moving X-axis
 
     // Update is called once per frame
     void Update()
     {
         transform.localPosition += valueToMove;
 
-        // if too far forward
-        if (transform.localPosition.z > 5)
+        // // if too far forward
+        // if (transform.localPosition.z > 5)
+        // {
+        //     valueToMove.z = -0.01f;     // moves object backwards
+        // }
+
+        // // if too far backward
+        // if (transform.localPosition.z < -5)
+        // {
+        //     valueToMove.z = 0.01f;      // moves object forward
+        // }
+
+        // changing direction to X (Task 2)
+        if (transform.localPosition.x > 5)
         {
-            valueToMove.z = -0.01f;     // moves object backwards
+            valueToMove.x = -0.01f;    
         }
 
-        // if too far backward
-        if (transform.localPosition.z < -5)
+        if (transform.localPosition.x < -5)
         {
-            valueToMove.z = 0.01f;      // moves object forward
+            valueToMove.x = 0.01f;     
         }
 
         print(transform.localPosition.x);
